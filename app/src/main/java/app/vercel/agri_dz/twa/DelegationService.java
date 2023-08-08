e39d55e37e82646bbb58706de66837fc6406c5b6
hamzamamadi2001 <hamzamamadi20@gmail.com>
2023-08-08 17:18:50 +0100

    fixing the authproblem

---
+++ app/src/main/java/app/vercel/agri_dz/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package app.vercel.agri_dz.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
